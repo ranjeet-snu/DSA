e8cca08a713c2f51d1189d76b093e1bc58dabc28
ranjeet-snu <107781937+ranjeet-snu@users.noreply.github.com>
2022-07-21 22:47:13 +0530

    Update BinarySearch.cpp

--- C++/BinarySearch.cpp
+++ C++/BinarySearch.cpp
@@ -1,1 +1,46 @@
-djf
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int BinarySearch(int arr[],int n,int key)
+{
+   int s=0;
+   int e=n;
+   
+   while (s<=e)
+   { int mid=(s+e)/2;
+     if (key==arr[mid])
+        { return mid; }
+        
+    else if( key<arr[mid])
+    {  e= mid-1;}
+    else
+    { s= mid+1;}
+   
+   }
+   return -1;
+}
+int main() 
+{ int n;
+   cout<<"Enter array Size:";
+   cin>>n;
+   
+   int arr[n];
+   for ( int i=0;i<n;i++)
+   {
+       cout<<"Enter Elemenet for index ["<<i<<"] :";
+       cin>>arr[i];
+   }
+   int key;
+   cout<<"Enter Key to be searched: ";
+   cin>>key;
+   
+   cout<<BinarySearch(arr,n,key);
+   
+   
+   
+
+
+
+   return 0;
+}
