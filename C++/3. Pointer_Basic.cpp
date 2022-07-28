018b2593a135b8d9bacb96697b122920597f74c4
ranjeet-snu <107781937+ranjeet-snu@users.noreply.github.com>
2022-07-28 15:45:40 +0530

    Update 3. Pointer_Basic.cpp

--- C++/3. Pointer_Basic.cpp
+++ C++/3. Pointer_Basic.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int main() 
 { int n;
   n=8;
-  int *n_ptr;
+  int *n_ptr;                                             // Pointer --- used for store address of variable of same type;
+                                                         // Pointers always takes 8 byte;
   n_ptr=&n;
   cout<<"value of n: "<<n<<endl;
-  cout<<"value of n with *n_ptr: "<<*n_ptr<<endl;           // pointer can access value too
+  cout<<"value of n with *n_ptr: "<<*n_ptr<<endl;           // *ptr become exactly same as n , as it has same address 
   
   cout<<"Address of n: "<<&n<<endl;
   cout<<"Address of n with n_ptr: "<<n_ptr<<endl;
